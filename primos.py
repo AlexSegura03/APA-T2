b3af1c5771ea4574e21202936587184676aa35f9
Alex Segura <alex.segura.medina@estudiantat.upc.edu>
2025-03-22 20:59:44 +0100

    funcion 2 y 3 añadidas

--- primos.py
+++ primos.py
@@ -1,3 +1,5 @@
+## FUNCION ES PRIMO
+
 """
 Àlex Segura Medina
 
@@ -36,12 +38,36 @@ if __name__ == "__main__":
     import doctest
     doctest.testmod()
     
+## ----------------------------------------------------------------------
 
+## FUNCIÓN PRIMOS
 
+"""
+Àlex Segura Medina
+
+>>> primos(10)
+(2, 3, 5, 7)
+
+>>> primos(17)
+(2, 3, 5, 7, 11, 13)
+
+>>> primos(45)
+(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43)
+
+"""
 
 def primos(numero):
     """
+
     Devuelve una tupla con todos los números primos menores que su argumento."
+
+    >>> lista = []
+    >>> for numero in range(2, 20):
+    ...     if esPrimo(numero):
+    ...     lista.append(numero)
+    ... return tuple(lista)
+
+   (2, 3, 5, 7, 11, 13, 17, 19)
     
     """
     lista = []
@@ -49,6 +75,50 @@ def primos(numero):
         if esPrimo(i):
             lista.append(i)
     return tuple(lista)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+
+## ----------------------------------------------------------------------
+
+## FUNCIÓN DESCOMPONER PRIMOS
+
+""""
+Àlex Segura Medina
+
+>>> descompon()
+
+"""
+
+def descompon(numero):
+    """
+    Devuelve una tupla con la descomposición en factores primos de su argumento.
+
+    >>> descompon(12)
+    2, 2, 3
+   
+    >>> descompon(45)
+    3, 3, 5
+
+    >>> descompon(72)
+    2, 2, 2, 3, 3
+
+    """
+    factores = []
+    divisor = 2
+    
+    def descompon(numero):
+        factores = []
+        divisor = 2
+            while numero > 1:
+                while numero % divisor == 0:
+                    factores.append(divisor)
+                    numero //= divisor
+                divisor += 1
+        return tuple(factores)
 
 if __name__ == "__main__":
     import doctest
